196c913e0b0b45dbcacc205a8f02c0296c63996a
Diego Tarzona <diego126.tn@gmail.com>
2017-12-21 23:01:45 -0500

    Instalacion del proyecto completado con éxito

---
+++ sigi_backend/src/main/java/pe/gob/sunat/sigi_backend/App.java
@@ -0,0 +1,13 @@
+package pe.gob.sunat.sigi_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
